6446006495df6012873041e0b190c567dc511318
Javi H. Gil <javihgil@softspring.es>
2026-06-18 11:58:30 +0200

    Fix code style

--- src/DependencyInjection/SfsNotificationExtension.php
+++ src/DependencyInjection/SfsNotificationExtension.php
@@ -5,8 +5,8 @@ namespace Softspring\NotificationBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SfsNotificationExtension extends Extension
 {
--- src/Model/NotificationInterface.php
+++ src/Model/NotificationInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Softspring\NotificationBundle\Model;
 
 use DateTime;
